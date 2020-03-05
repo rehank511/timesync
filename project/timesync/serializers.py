2654ef6cfa12d65056f0bdd39920ec07f9c29bfe
Devin French <d_french3@u.pacific.edu>
2020-03-04 22:33:06 -0800

    Add serializers

---
+++ project/timesync/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import User, Calendar, Event
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('user_id', 'username', 'email')
+
+
+class CalendarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Calendar
+        fields = ('calendar_id', 'user_id', 'name', 'events')
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ('event_id', 'calendar_id', 'name',
+                  'location', 'start', 'end', 'description')
