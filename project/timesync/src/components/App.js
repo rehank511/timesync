d7adf5577fd5b65b0a3fb2f3370e961af4b76d1a
Devin <devinfrench@users.noreply.github.com>
2020-03-05 10:49:20 -0800

    Add fullcalendar

--- project/timesync/src/components/App.js
+++ project/timesync/src/components/App.js
@@ -1,63 +1,71 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+import "../styles/main.scss";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            loaded: false,
-            placeholder: "Loading"
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false,
+      placeholder: "Loading"
+    };
+  }
 
-    componentDidMount() {
-        fetch("api/event")
-            .then(response => {
-                if (response.status > 400) {
-                    return this.setState(() => {
-                        return { placeholder: "Something went wrong!" };
-                    });
-                }
-                return response.json();
-            })
-            .then(data => {
-                this.setState(() => {
-                    return {
-                        data,
-                        loaded: true
-                    };
-                });
-            });
-    }
+  componentDidMount() {
+    fetch("api/event")
+      .then(response => {
+        if (response.status > 400) {
+          return this.setState(() => {
+            return { placeholder: "Something went wrong!" };
+          });
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState(() => {
+          return {
+            data,
+            loaded: true
+          };
+        });
+      });
+  }
 
-    render() {
-        return (
-            <ul>
-                {this.state.data.map(event => {
-                    return (
-                        <li key={event.event_id}>
-                            {event.name}
-                            <ul>
-                                <li>
-                                    <b>Location:</b> {event.location}
-                                </li>
-                                <li>
-                                    <b>Start:</b> {event.start}
-                                </li>
-                                <li>
-                                    <b>End:</b>{event.end}
-                                </li>
-                                <li>
-                                    <b>Description:</b> {event.description}
-                                </li>
-                            </ul>
-                        </li>
-                    );
-                })}
-            </ul>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.data.map(event => {
+            return (
+              <li key={event.event_id}>
+                {event.name}
+                <ul>
+                  <li>
+                    <b>Location:</b> {event.location}
+                  </li>
+                  <li>
+                    <b>Start:</b> {event.start}
+                  </li>
+                  <li>
+                    <b>End:</b>
+                    {event.end}
+                  </li>
+                  <li>
+                    <b>Description:</b> {event.description}
+                  </li>
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+        <FullCalendar defaultView="dayGridMonth" plugins={[dayGridPlugin]} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- project/timesync/webpack.config.js
+++ project/timesync/webpack.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
-        }]
-    }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
 };
